f7f21705ad0eaa73724d28633c4033abbd744c47
cosminghr <100318817+cosminghr@users.noreply.github.com>
2024-05-27 16:52:10 +0300

    mici modif

--- EmailSender2/src/main/java/com/example/emailsender2/controller/EmailController.java
+++ EmailSender2/src/main/java/com/example/emailsender2/controller/EmailController.java
@@ -1,27 +1,22 @@
 package com.example.emailsender2.controller;
 
-
-import com.example.emailsender2.dtos.EmailDTO;
 import com.example.emailsender2.dtos.EmailUserDTO;
 import com.example.emailsender2.entities.Email;
 import com.example.emailsender2.service.EmailService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/emails")
 public class EmailController {
 
     public static final String FIRST_TOKEN = "6d91f453-b188-4dda-b320-3af63b1c4d01"; // admin id
-    public static final String SECOND_TOKEN = "42b00d5f-6a77-4c46-ba35-368a68131e4a"; //client id
+    public static final String SECOND_TOKEN = "42b00d5f-6a77-4c46-ba35-368a68131e4a"; // client id
 
     private final EmailService emailService;
 
@@ -29,16 +24,14 @@ public class EmailController {
     public EmailController(EmailService emailService) {
         this.emailService = emailService;
     }
+
     Logger logger = LogManager.getLogger(EmailController.class);
 
     @PostMapping("/sending-email")
-    public ResponseEntity<EmailUserDTO> sendingEmail(@RequestBody EmailUserDTO emailDto) {
-        emailService.sendEmailUser(emailDto);
-        return new ResponseEntity<>(emailDto, HttpStatus.OK);
+    public ResponseEntity<Boolean> sendingEmail(
+            @RequestPart("emailRequest") Email email,
+            @RequestPart("file") MultipartFile file) {
+        emailService.sendEmailRezervare1(email, file);
+        return new ResponseEntity<>(true, HttpStatus.OK);
     }
-
-
-
-
-
 }
--- EmailSender2/src/main/java/com/example/emailsender2/service/EmailService.java
+++ EmailSender2/src/main/java/com/example/emailsender2/service/EmailService.java
@@ -8,17 +8,17 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.transaction.Transactional;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
     private JavaMailSender emailSender;
-
     private final EmailRepository emailRepository;
 
     public EmailService(JavaMailSender emailSender, EmailRepository emailRepository) {
@@ -26,7 +26,7 @@ public class EmailService {
         this.emailRepository = emailRepository;
     }
 
-    public EmailUserDTO sendEmailUser(EmailUserDTO emailUserDTO) {
+    public EmailUserDTO sendEmailUser(EmailUserDTO emailUserDTO, MultipartFile file) {
         try {
             MimeMessage mimeMessage = emailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
@@ -39,7 +39,7 @@ public class EmailService {
             // Create the HTML content for the email
             String htmlContent = "<!DOCTYPE html>\n" +
                     "<html lang=\"en\">\n" +
-                    "<head>\n" +
+                    "<head style=\"background-color: lime; color: black;\">\n" +
                     "    <meta charset=\"UTF-8\">\n" +
                     "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
                     "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
@@ -47,21 +47,27 @@ public class EmailService {
                     "</head>\n" +
                     "<body>\n" +
                     "    <div style=\"font-family: Arial, sans-serif; font-size: 14px;\">\n" +
-                    "        <h2 style=\"color: #333;\">Dear " + emailUserDTO.getName() + ",</h2>\n" +
+                    "        <h2 style=\" background-color: lime; color: black;\">Dear " + emailUserDTO.getName() + ",</h2>\n" +
                     "        <p style=\"color: #333;\">You have successfully created your account.</p>\n" +
                     "    </div>\n" +
+                    "    <img src=\"https://images.pexels.com/photos/913215/pexels-photo-913215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1\" alt=\"Welcome Image\" style=\"display: block; margin: auto;\">\n" +
                     "</body>\n" +
                     "</html>";
 
             // Set email content
             helper.setText(htmlContent, true);
 
+            // Attach the file if it exists
+            if (file != null && !file.isEmpty()) {
+                helper.addAttachment(file.getOriginalFilename(), file);
+            }
+
             // Send the email
             emailSender.send(mimeMessage);
 
             return emailUserDTO;
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
+        } catch (MessagingException m ) {
+            throw new RuntimeException("Failed to send email", m);
         }
     }
 
@@ -117,7 +123,73 @@ public class EmailService {
         } catch (MessagingException e) {
             // Handle messaging exception
             email.setEmailStatus(StatusEmail.ERROR);
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to send email", e);
+        } catch (MailException e) {
+            // Handle mail exception
+            email.setEmailStatus(StatusEmail.ERROR);
+        }
+
+        // Save and return the email
+        return emailRepository.save(email);
+    }
+    @Transactional
+    public Email sendEmailRezervare1(Email email, MultipartFile file) {
+        email.setEmailDate(LocalDateTime.now());
+        try {
+            MimeMessage mimeMessage = emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+            // Set email details
+            helper.setFrom(email.getEmailFrom());
+            helper.setTo(email.getEmailTo());
+            helper.setSubject(email.getTitle());
+
+            // Create the HTML content for the email using the template
+            String htmlContent = "<!DOCTYPE html>\n" +
+                    "<html lang=\"en\">\n" +
+                    "<head>\n" +
+                    "    <meta charset=\"UTF-8\">\n" +
+                    "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                    "    <title>Email Template</title>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "    <div style=\"font-family: Arial, sans-serif; font-size: 14px;\">\n" +
+                    "    <img src=\"https://images.pexels.com/photos/913215/pexels-photo-913215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1\" alt=\"Welcome Image\" style=\"display: block; margin: auto;\">\n" +
+                    "        <h2 style=\"color: #333;\">Dear " + email.getName() + ",</h2>\n" +
+                    "        <p style=\"color: #333;\">Your reservation with the id: " + email.getReservationId() + " was successfully created.</p>\n" +
+                    "        <p style=\"color: #333;\">You can see below some information about your trip with us:</p>\n" +
+                    "        <ul style=\"color: #333;\">\n" +
+                    "            <li><strong>Name:</strong> " + email.getName() + "</li>\n" +
+                    "            <li><strong>Email:</strong> " + email.getEmailTo() + "</li>\n" +
+                    "            <li><strong>Start Date:</strong> " + email.getStartDate() + "</li>\n" +
+                    "            <li><strong>End Date:</strong> " + email.getEndDate() + "</li>\n" +
+                    "            <li><strong>Room Numbers:</strong> " + email.getRoomNumbers() + "</li>\n" +
+                    "            <li><strong>Final Cost:</strong> " + email.getReservationCost() + "</li>\n" +
+                    "        </ul>\n" +
+                    "        <p style=\"color: #333;\">Thank you for choosing us! See you soon!</p>\n" +
+                    "        <p style=\"color: #333;\">Best Regards,<br/>" + email.getOwnerRef() + "</p>\n" +
+                    "    </div>\n" +
+                    "</body>\n" +
+                    "</html>";
+
+            // Set email content
+            helper.setText(htmlContent, true);
+
+            // Attach the file if it exists
+            if (file != null && !file.isEmpty()) {
+                helper.addAttachment(file.getOriginalFilename(), file);
+            }
+
+            // Send the email
+            emailSender.send(mimeMessage);
+
+            // Update email status
+            email.setEmailStatus(StatusEmail.SENT);
+        } catch (MessagingException e) {
+            // Handle messaging exception
+            email.setEmailStatus(StatusEmail.ERROR);
+            throw new RuntimeException("Failed to send email", e);
         } catch (MailException e) {
             // Handle mail exception
             email.setEmailStatus(StatusEmail.ERROR);
@@ -127,3 +199,4 @@ public class EmailService {
         return emailRepository.save(email);
     }
 }
+
